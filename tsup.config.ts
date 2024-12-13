73c110ae9f14c7c26521accdc8f1580f87ebc724
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-13 10:30:05 +0700

    Improved tsconfig.json and added tsup build file.

---
+++ tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs"],
+    outDir: "dist/cjs",
+    dts: false,
+    sourcemap: true,
+    clean: true,
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    outDir: "dist/esm",
+    dts: false,
+    sourcemap: true,
+    clean: false, // Don't delete the dist because another file has already been created.
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    outDir: "dist/types",
+    dts: true,
+    sourcemap: false,
+    clean: false,
+  },
+]);
